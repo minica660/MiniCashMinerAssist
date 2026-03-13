23542c1ee059d323117961721596a0adabf6069c
MiniCash <tminecraft2143@gmail.com>
2026-03-13 22:04:29 +0900

    Add GUI class implementing InventoryHolder

---
+++ src/main/java/MiniCash/GUI.java
@@ -0,0 +1,9 @@
+package MiniCash;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public class GUI implements InventoryHolder {
+    @Override
+    public Inventory getInventory() { return null; }
+}
